fe9e0ec3a021251c82a1d509812867f438685ffc
George Marcell <georgemarcell5@gmail.com>
2020-04-24 20:58:17 +0700

    Tambah foto

--- PA-Chill-Penguin-cuy/Form1.vb
+++ PA-Chill-Penguin-cuy/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
-    Dim Normal As Image = Image.FromFile("C:\Users\georg\source\repos\JulioYosafat98\Penguin-PA\Penguin PA\Normal.jpg")
-    Dim bg As Image = Image.FromFile("C:\Users\georg\source\repos\JulioYosafat98\Penguin-PA\Penguin PA\Bg.jpg")
+    Dim Normal As Image = Image.FromFile("C:\Users\georg\source\repos\PA-Chill-Penguin-cuy\Normal.jpg")
+    Dim bg As Image = Image.FromFile("C:\Users\georg\source\repos\PA-Chill-Penguin-cuy\Bg.jpg")
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -8,7 +8,7 @@
 
     Private Sub Pb_Paint(sender As Object, e As PaintEventArgs) Handles Pb.Paint
         Dim GFX As Graphics = e.Graphics
-        GFX.DrawImage(bg, 100, 100, bg.Width * 5, bg.Height * 2)
-        GFX.DrawImage(bg, 0, 210)
+        GFX.DrawImage(bg, 0, 0, bg.Width * 5, bg.Height * 2)
+        GFX.DrawImage(Normal, 0, 210)
     End Sub
 End Class
