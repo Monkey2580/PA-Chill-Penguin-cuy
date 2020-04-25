00df8eb3af8303e6519b07504185e22cee17b06f
George Marcell <georgemarcell5@gmail.com>
2020-04-25 22:01:14 +0700

    Heheheheh

--- PA-Chill-Penguin-cuy/Form1.Designer.vb
+++ PA-Chill-Penguin-cuy/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,9 +20,13 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Pb = New System.Windows.Forms.PictureBox()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.ImgList1 = New System.Windows.Forms.ImageList(Me.components)
         CType(Me.Pb, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -33,6 +37,17 @@ Partial Class Form1
         Me.Pb.Size = New System.Drawing.Size(1500, 1500)
         Me.Pb.TabIndex = 0
         Me.Pb.TabStop = False
+        '
+        'Timer
+        '
+        '
+        'ImgList1
+        '
+        Me.ImgList1.ImageStream = CType(resources.GetObject("ImgList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImgList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImgList1.Images.SetKeyName(0, "Normal1.jpg")
+        Me.ImgList1.Images.SetKeyName(1, "Slides0.jpg")
+        Me.ImgList1.Images.SetKeyName(2, "Slides1.jpg")
         '
         'Form1
         '
@@ -48,4 +63,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Pb As PictureBox
+    Friend WithEvents Timer As Timer
+    Friend WithEvents ImgList1 As ImageList
 End Class
--- PA-Chill-Penguin-cuy/Form1.vb
+++ PA-Chill-Penguin-cuy/Form1.vb
@@ -1,30 +1,97 @@
 ﻿Public Class Form1
-
-    Dim Normal As Image = Image.FromFile("C:\Users\georg\source\repos\PA-Chill-Penguin-cuy\Normal.png")
+    Dim Slides As Bitmap = Image.FromFile("C:\Users\georg\source\repos\PA-Chill-Penguin-cuy\Slides0.jpg")
+    Dim Slides1 As Bitmap = Image.FromFile("C:\Users\georg\source\repos\PA-Chill-Penguin-cuy\Slides1.jpg")
+    Dim Normal As Bitmap = Image.FromFile("C:\Users\georg\source\repos\PA-Chill-Penguin-cuy\Normal1.jpg")
     Dim bg As Image = Image.FromFile("C:\Users\georg\source\repos\PA-Chill-Penguin-cuy\Bg.jpg")
-
+    Dim GFX As Graphics
+    Dim tick As Integer = 0
+    Dim steps As Integer = 70
+    Dim direction As Char = "l"
+    Dim action As String = "idle"
+    Dim a As Integer = steps
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Private Sub Pb_Paint(sender As Object, e As PaintEventArgs) Handles Pb.Paint
-        Dim GFX As Graphics = e.Graphics
+
+        GFX = e.Graphics
         GFX.DrawImage(bg, 0, 0, bg.Width * 5, bg.Height * 2)
-        GFX.DrawImage(Normal, 70, 300, Normal.Width, Normal.Height)
+        Normal.MakeTransparent(Color.White)
+        '' ujung stage 1120 x coordinate nya
+
+        Timer.Start()
+
+        If action = "idle" Then
+            GFX.DrawImage(Normal, steps, 300, Normal.Width, Normal.Height)
+        End If
+
+        If action = "slide" Then
+            If tick = 1 Then
+
+                GFX.DrawImage(Slides, steps, 300, Normal.Width, Normal.Height)
+                Slides.MakeTransparent()
+
+            Else
+                If a < 1120 Then
+                    If direction = "l" Then
+                        Slides1.RotateFlip(RotateFlipType.Rotate180FlipY)
+                        direction = "r"
+                    End If
+                    GFX.DrawImage(Slides1, steps, 300, Normal.Width, Normal.Height)
+                    Slides1.MakeTransparent()
+                    steps += 10
+                    a = steps
+                Else if a > 70 then  
+                    If direction = "r" Then
+                        Slides1.RotateFlip(RotateFlipType.Rotate180FlipY)
+                        direction = "l"
+                    End If
+                    GFX.DrawImage(Slides1, steps, 300, Normal.Width, Normal.Height)
+                    Slides1.MakeTransparent()
+                    steps -= 10
+                End If
+                End If
+        End If
+
     End Sub
 
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        tick += 1
+        Pb.Refresh()
 
+    End Sub
 
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
-        If e.KeyCode = Keys.Right Then
 
-            Normal.RotateFlip(RotateFlipType.Rotate180FlipY)
+        If e.KeyCode = Keys.Right Then
+            If direction = "l" Then
+                Normal.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+            direction = "r"
+            steps += 10
             Refresh()
         End If
         If e.KeyCode = Keys.Left Then
-
-            Normal.RotateFlip(RotateFlipType.Rotate180FlipY)
+            If direction! = "r" Then
+                Normal.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+            direction = "l"
+            steps -= 10
             Refresh()
         End If
+        If e.KeyCode = Keys.S Then
+            action = "slide"
+            tick = 1
+            Refresh()
+        End If
+
+        If e.KeyCode = Keys.T Then
+            action = "idle"
+
+            Refresh()
+        End If
+
     End Sub
+
 End Class
